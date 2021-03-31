aadd5df6fd3c5b0d01eb57cb9fec8490c9ca6ee3
psicolambo <breno.maia@edu.unirio.br>
2021-03-31 20:27:47 -0300

    new functions on Nba.js

--- src/components/Nba.js
+++ src/components/Nba.js
@@ -1,17 +1,45 @@
 import {getTodayGames} from "../contexts/NBAContext"
-import React, {Component} from "react";
+import React, {Component, useState, useEffect} from "react";
 import NavBar from "./NavBar";
+import { firestore } from "../firebase"
 
-class Nba extends Component {
+export default function Nba() {
   
-  render() {
-    // console.log(getTodayGames())
-    return (
-      <>
-        <NavBar></NavBar>
-      </>
-    );
-  } 
-}
+  const [ todayGames, setTodayGames ] = useState([])
+  const [ userData, setUserData ] = useState({})
+ 
+  async function getGames(){
+    let games = await getTodayGames()
+    setTodayGames(games)
+    console.log(games)
+  }
 
-export default Nba;
+  //async function getUserdata(){
+   // await firestore.collection("users")
+   // .doc("" + currentUser.uid).get().then((document) => {
+   // setUserData(document.data())
+   //   });
+  //}
+
+  useEffect(() => {
+    //getUserdata()
+    getGames()
+  },[])
+
+  return (
+    <>
+      <NavBar></NavBar>
+      {todayGames.map(game => (
+        <div>
+          <div>
+            {game.hTeam.fullName}
+          </div>
+          <div>
+            {game.vTeam.fullName}
+          </div>
+       </div>
+      ))} 
+    </>
+  );
+   
+}
